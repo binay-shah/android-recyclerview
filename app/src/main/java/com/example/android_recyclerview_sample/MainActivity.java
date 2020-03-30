d5ed72fe8359cd7cf87c0e6834ea02fdf2223756
binay-shah <binay18.shah@gmail.com>
2020-03-30 09:45:47 +0545

    Step.01-Exercise-Add-a-RecyclerView

--- app/src/main/java/com/example/android_recyclerview_sample/MainActivity.java
+++ app/src/main/java/com/example/android_recyclerview_sample/MainActivity.java
@@ -4,14 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.util.Log;
 
 
-import com.example.android_recyclerview_sample.adapter.SandwichAdapter;
 import com.example.android_recyclerview_sample.model.Sandwich;
 import com.example.android_recyclerview_sample.utils.JsonUtils;
-
-import org.json.JSONException;
 
 import java.util.ArrayList;
 
@@ -19,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Sandwich> sandwichList ;
     private RecyclerView recyclerView;
-    private SandwichAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,32 +24,26 @@ public class MainActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
 
-        Log.d("debug-array", "" + sandwiches.length);
-
         sandwichList = new ArrayList<Sandwich>();
 
         for (String item: sandwiches){
 
                 sandwichList.add(JsonUtils.parseSandwichJson(item));
-
         }
 
-        Log.d("debug-list", "" + sandwichList.size());
-
-        recyclerView = findViewById(R.id.sandwich_list);
-
-        // TODO (07) Create a new SandwichAdapter variable,
-        // and bind it to the RecyclerView’s Adapter.
-
-        adapter = new SandwichAdapter(this);
-        recyclerView.setAdapter(adapter);
-
-        adapter.setData(sandwichList);
-
-
-
     }
-
-
-
 }
+
+// TODO (02) Create SandwichAdapter class and extend it
+// from RecyclerView.Adapter<TextItemViewHolder>
+
+// TODO (03) Create a variable, data, that holds a list of Sandwich.
+
+// TODO (04) Override getItemCount() to return the total number of items in the data set.
+
+// TODO (05) Override onBindViewHolder() and have it update the contents of the
+// ViewHolder to reflect the item at the given position.
+
+// TODO (06) Override onCreateViewHolder()
+
+// TODO (07) Create a new SandwichAdapter variable,
